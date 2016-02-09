4f09239c803c416bda087ba535cc4676cefe8031
mse2321 <mse233@gmail.com>
2016-02-09 18:18:34 -0500

    finished Unit 2 Lesson 1

--- app/owm-app.js
+++ app/owm-app.js
@@ -1,17 +1,23 @@
-angular.module('OWMApp', ['ngRoute'])
-	.config(['$routeProvider', function($routeProvider){
+angular.module('OWMApp', ['ngRoute', 'ngAnimate'])
+	.value('ownCities', ['New York', 'Dallas', 'Chicago'])
+
+		.controller('HomeCtrl', function($scope){
+			//empty for noew
+		})
+
+	.config(function($routeProvider){
 		$routeProvider.when('/', {
-			templateUrl: 'home.html',
+			templateUrl: './home.html',
 			controller: 'HomeCtrl'
 		})
 		.when('/cities/:city', {
-			templateUrl: 'city.html',
+			templateUrl: './city.html',
 			controller: 'CityCtrl',
 			resolve: {
 				city: function(ownCities, $route, $location) {
 					var city = $route.current.params.city;
-					if(ownCities.indexOf(city) === -1) {
-						$location.path('/error')
+					if(ownCities.indexOf(city) == -1) {
+						$location.path('/error');
 						return;
 					}
 					return city;
@@ -20,18 +26,24 @@ angular.module('OWMApp', ['ngRoute'])
 		})
 		.when('/error',  {
 			template: '<p>Error - Page Not Found</p>'
-		})
-		.run(function($routeScope, $location){
-			$rootScope.$on('$routeChangeError', function() {
-				$location.path('/error');
-			})
 		});
 		
-	}])
-	.value('ownCities', ['New York', 'Dallas', 'Chicago'])
-	.controller('HomeCtrl', ['$scope', function($scope){
-		//empty for noew
-	}])
-	.controller('CityCtrl', ['$scope', function($scope, city){
+	})
+
+	.controller('CityCtrl', function($scope, city){
 		$scope.city = city;
-	}]);
+	})
+
+	.run(function($rootScope, $location, $timeout) {
+		$rootScope.$on('$routeChangeError', function() {
+			$location.path("/error");
+		});
+		$rootScope.$on('$routeChangeStart', function() {
+			$rootScope.isLoading = true;
+		});
+		$rootScope.$on('$routeChangeSuccess', function() {
+			$timeout(function() {
+			    $rootScope.isLoading = false;
+			}, 1000);
+		})
+	})
